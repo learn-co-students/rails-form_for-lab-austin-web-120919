e754bb3dd64802f189f4f5767c2842ed79211093
gwhitlock8 <gavinswhitlock@gmail.com>
2020-01-14 14:27:19 -0600

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,39 @@
+class StudentsController < ApplicationController
+  before_action :find_student, only: [:edit,:show,:update]
+  
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+      @student = Student.new(student_params(:first_name,:last_name))
+      @student.save
+      redirect_to student_path(@student)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+      @student.update(student_params(:first_name,:last_name))
+      redirect_to student_path(@student)
+  end
+
+  private
+
+  def find_student
+      @student = Student.find(params[:id])
+  end
+
+  def student_params(*args)
+      params.require(:student).permit(*args)
+  end
+
+end
